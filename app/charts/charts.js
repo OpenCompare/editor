6ac3ed3e00b6bb677155086a75ed7dbe8ebd7267
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-12-01 14:56:09 +0100

    refactor charts to adopt the new directive implementation

--- app/charts/charts.js
+++ app/charts/charts.js
@@ -4,8 +4,26 @@
 
 angular
   .module('openCompareEditor')
-  .directive('openCompareCharts', function() {
+  .directive('ocCharts', function() {
         return {
-            templateUrl: 'charts/charts.html'
+          restrict: 'E',
+          require: "^ocEditor",
+          scope: {},
+          templateUrl: 'charts/charts.html',
+          controller: "ChartsCtrl",
+          link: function($scope, element, attrs, ctrl) {
+
+            $scope.pcmContainer = ctrl.pcmContainer;
+
+            $scope.$watch("pcmContainer.pcm", function(newVal) {
+              $scope.pcm = ctrl.pcmContainer.pcm;
+              $scope.metadata = ctrl.pcmContainer.metadata;
+              $scope.id = ctrl.pcmContainer.id;
+            });
+
+            $scope.config = ctrl.config;
+            $scope.state = ctrl.state;
+
+          }
         };
     });
