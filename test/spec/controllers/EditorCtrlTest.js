9862aa4c53581eb0fb663e4a93161d90711e83c3
Mathieu Acher <mathieu.acher@irisa.fr>
2016-11-06 16:35:58 +0100

    openCompareEditorApp now reads an external JSON file

--- test/spec/controllers/EditorCtrlTest.js
+++ test/spec/controllers/EditorCtrlTest.js
@@ -11,14 +11,14 @@ describe('Controller: EditorCtrl', function () {
 
 
  var controller;
-
+ var scope = {};
 
   // Initialize the controller and a mock scope
 
   beforeEach(inject(function ($injector) {
 
     controller = $injector.get('$controller');
-    // scope = $injector.get('rootScope').$new();
+    //scope = $injector.get('rootScope').$new();
     //controller = $_controller_('TestCtrl', {
       //scope: scope
       // place here mocked dependencies
@@ -28,24 +28,35 @@ describe('Controller: EditorCtrl', function () {
 
 
 
-  it('do nothing', inject(function ($rootScope, $timeout) {
+  it('do nothing', inject(function ($httpBackend, $timeout) {
     //expect(controller..length).toBe(3);
+
+
 
     console.log('Hello, world!');
 
     var a = true;
     expect(a).toBe(true);
 
-    var scope = $rootScope.$new();
+    var pcm = {};
+    //$httpBackend.whenGET("foopcm1.json").passThrough();
+
+    var timeout = {};
     var controllerBis = controller('TestCtrl', { $scope: scope });
 
-    //  httpBackend.flush();
+//    $httpBackend.flush();
+
     console.log('State: ' + scope.myState.saved);
     // TODO: fix timeout issue
-    $timeout.flush();
+
 
     console.log('PCM: ' + scope.myPCMContainer.pcm);
     console.log('PCM: ' + scope.myPCMContainer.pcm.name);
+
+
+    $timeout.flush();
+
+
 
 
   }));
