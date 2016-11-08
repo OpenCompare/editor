940207a53cb03ad5ce159ec1a7b2303b012e2193
Mathieu Acher <mathieu.acher@irisa.fr>
2016-11-08 12:13:17 +0100

    trying to better configure testing and launch of app

--- app/openCompareEditorApp.js
+++ app/openCompareEditorApp.js
@@ -13,8 +13,6 @@ angular
   .module('openCompareEditorApp', ['openCompareEditor'])
   // getPCM (service)
   .controller('TestCtrl', function($scope, $http, $q, pcmApi, $timeout, openCompareServer) {
-    console.log('cioucou');
-    console.log('cioucou http' + $http);
 
     $scope.myPCMContainer = {
 
@@ -27,85 +25,100 @@ angular
 
     $scope.isComingFromTest = false;
 
-  //  $scope.pcmLocation = ''; // 'test/foopcm1.json';
+//    $scope.pcmLocation = ''; // 'test/foopcm1.json';
 
 
 
     $scope.myConfig = {
-      serverMode: "local" // remote is the alternative 
+      serverMode: 'local', // "remote" is the other mode
+      pcmlocation: 'test/foopcm1.json'
     };
+
+
+// example with remote mode
+/*
+    $scope.myConfig = {
+      serverMode: 'remote' // "remote" is the other mode
+      serverAddress: 'http://localhost:9000';
+      id: "581b53b38d6a4d31e94f5d38";
+    };*/
 
     ////// With Local PCM in an external JSON //////
     if ($scope.myConfig.serverMode === "local") {
 
-
-      var pcmLocation = 'test/foopcm1.json';
-      if ($scope.pcmLocation !== undefined) {
-          pcmLocation = $scope.pcmLocation;
+     var pcmLocation = $scope.myConfig.pcmlocation ; //'' ; // test/foopcm1.json';
+    // if ($scope.myConfig.pcmlocation !== undefined) {
+     if ($scope.pcmlocation !== undefined) {
+          pcmLocation = $scope.pcmlocation;
           $scope.isComingFromTest = true;
-        }
+      }
+
 
       var canceler = $q.defer();
+      if (pcmLocation.length > 0) {
+        $http.get(pcmLocation).success(function(data) {
+            console.log('data from GET foopcm', data);
+              var myPcm = data.pcm;
+              var container = {};
+              console.log('data: ' + data);
+              console.log('myPCM: ' + myPcm);
+              container.pcm = pcmApi.loadPCMModelFromString(JSON.stringify(myPcm));
+              // container.pcm = pcmApi.loadPCMModelFromString(JSON.stringify(container.pcm));
+              pcmApi.decodePCM(container.pcm);
 
-
-      $http.get(pcmLocation).success(function(data) {
-          console.log('data from GET foopcm', data);
-            var myPcm = data.pcm;
-            var container = {};
-            console.log('data: ' + data);
-            console.log('myPCM: ' + myPcm);
-            container.pcm = pcmApi.loadPCMModelFromString(JSON.stringify(myPcm));
-            // container.pcm = pcmApi.loadPCMModelFromString(JSON.stringify(container.pcm));
-            pcmApi.decodePCM(container.pcm);
-
-            $timeout(function() {
-              $scope.myPCMContainer.pcm = container.pcm;
-              $scope.myPCMContainer.metadata = container.metadata;
-             //$scope.csvApi.open();
-             //$scope.htmlApi.open();
-             //$scope.mediaWikiApi.open();
-           }, 1000);
-       });
+              $timeout(function() {
+                $scope.myPCMContainer.pcm = container.pcm;
+                $scope.myPCMContainer.metadata = container.metadata;
+               //$scope.csvApi.open();
+               //$scope.htmlApi.open();
+               //$scope.mediaWikiApi.open();
+             }, 1000);
+         });
+       }
 
      }
 
+    console.log('before REMOTE: ' + $scope.myConfig.serverMode);
 
     ////// With OpenCompare Server (remote) //////
     if ($scope.myConfig.serverMode === "remote") {
-        //
-        // $scope.myConfig = {
-        //    serverMode: "remote",
-        //    serverAddress: "http://localhost:9000" // cross-origin issue "https://opencompare.org"
-        //  };
-        // openCompareServer.useRemoteServer("http://localhost:9000"); // cross-origin issue "https://opencompare.org"
-        //
-        //
-        // var id = "581b53b38d6a4d31e94f5d38"
-        // // var id = "5667063678c2faf9781b6f64";
-        // if (typeof id !== 'undefined') {
-        // //   /* Load a PCM from database */
-        //    $scope.id = id;
-        //    $scope.loading = true;
-        //    //$scope.setEdit(false, false);
-        //    //$scope.updateShareLinks();
-        //    openCompareServer.get("/api/get/" + $scope.id).
-        //    then(function (response) {
-        //      var data = response.data;
-        //      var pcm = pcmApi.loadPCMModelFromString(JSON.stringify(data.pcm));
-        //      pcmApi.decodePCM(pcm); // Decode PCM from Base64
-        //
-        //      $scope.myPCMContainer.pcm = pcm;
-        //      $scope.myPCMContainer.metadata = data.metadata;
-        //      $scope.myPCMContainer.id = id;
-        //      $scope.myState.saved = true;
-        //
-        //    }, function(error) {
-        //      console.log(error);
-        //    })
-        //      .finally(function () {
-        //        $scope.loading = false;
-        //      })
-        //  }
+
+        console.log('after REMOTE');
+
+      /*  $scope.myConfig = {
+           serverMode: "remote",
+           serverAddress: "http://localhost:9000" // cross-origin issue "https://opencompare.org",
+           id: '' // PCM id
+         };*/
+        openCompareServer.useRemoteServer($scope.myConfig.serverAddress);
+
+
+        var id = $scope.myConfig.id; //"581b53b38d6a4d31e94f5d38"
+        // var id = "5667063678c2faf9781b6f64";
+        if (typeof id !== 'undefined') {
+        //   /* Load a PCM from database */
+           $scope.id = id;
+           $scope.loading = true;
+           //$scope.setEdit(false, false);
+           //$scope.updateShareLinks();
+           openCompareServer.get("/api/get/" + $scope.id).
+           then(function (response) {
+             var data = response.data;
+             var pcm = pcmApi.loadPCMModelFromString(JSON.stringify(data.pcm));
+             pcmApi.decodePCM(pcm); // Decode PCM from Base64
+
+             $scope.myPCMContainer.pcm = pcm;
+             $scope.myPCMContainer.metadata = data.metadata;
+             $scope.myPCMContainer.id = id;
+             $scope.myState.saved = true;
+
+           }, function(error) {
+             console.log(error);
+           })
+             .finally(function () {
+               $scope.loading = false;
+             })
+         }
       }
 
 
--- test/spec/controllers/EditorCtrlTest.js
+++ test/spec/controllers/EditorCtrlTest.js
@@ -10,37 +10,93 @@ describe('Controller: EditorCtrl', function () {
 
 
   var controller;
+  var scope = {};
+  var pcmFileLocation = 'test/foopcm2.json'; // scope.myConfig.pcmlocation; 'test/foopcm2.json'
+
+
+
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($injector, $rootScope) {
     controller = $injector.get('$controller');
+
+    //scope.pcmLocation = pcmFileLocation;
+    scope.myConfig = $rootScope.$new();
+    scope.myConfig.serverMode = "local";
+    // choose / FIX
+    scope.myConfig.pcmlocation = pcmFileLocation;
+    scope.pcmlocation = pcmFileLocation;
+
+    controller('TestCtrl', { $scope: scope });
+
+
   }));
 
 
 
   it('works for foopcm1', inject(function ($rootScope, $httpBackend, $timeout) {
-   var pcmFileLocation = 'test/foopcm2.json';
-   var scope = $rootScope.$new();
-   scope.pcmLocation = pcmFileLocation;
-   scope.myConfig = {'serverMode': "local" };
 
-   controller('TestCtrl', { $scope: scope });
 
-   $httpBackend.whenGET("" + pcmFileLocation).respond(readJSON('app/' + pcmFileLocation));
+    expect(scope.isComingFromTest).toBe(true);
 
-   expect(scope.isComingFromTest).toBe(true);
+    $httpBackend.whenGET("" + pcmFileLocation).respond(readJSON('app/' + pcmFileLocation));
+
+
+    console.log('State: ' + scope.myState.saved);
+    console.log('pcmLocation: ' + pcmFileLocation);
+
+    // TODO: fix timeout issue
+    $httpBackend.flush();
+    $timeout.flush();
+
+
+    console.log('PCM container: ' + scope.myPCMContainer);
+    console.log('PCM: ' + scope.myPCMContainer.pcm);
+    console.log('PCM: ' + scope.myPCMContainer.pcm.name);
+
+
+
+
+
+  }));
+
+
+/*
+  it('works for remote', inject(function ($rootScope, $httpBackend, $timeout, openCompareServer) {
+
+
+    var scope = $rootScope.$new();
+    scope.myConfig = {};
+    scope.myConfig.serverMode = "remote";
+    scope.myConfig.serverAddress = "http://localhost:9000";
+    scope.myConfig.id = "581b53b38d6a4d31e94f5d38";
+    scope.$apply();
+
+    console.log('serverMode : ' + scope.myConfig.serverMode);
+    controller('TestCtrl', { $scope: scope });
+
+   $httpBackend.whenGET(scope.myConfig.serverAddress + "/api/get/" + scope.myConfig.id).respond(
+      openCompareServer.get("/api/get/" + scope.myConfig.id)
+   );
+
 
    console.log('State: ' + scope.myState.saved);
    console.log('pcmLocation: ' + scope.pcmLocation);
+     console.log('myConfig : ' + scope.myConfig.serverMode);
 
-   // TODO: fix timeout issue
-   $httpBackend.flush();
-   $timeout.flush();
+   // TODO: fix
+   try {
+     $httpBackend.flush();
+     console.log('PCM container: ' + scope.myPCMContainer);
+     console.log('PCM: ' + scope.myPCMContainer.pcm);
+     console.log('PCM: ' + scope.myPCMContainer.pcm.name);
+   }
+   catch (e) {
+     // console.log(e);
+   }
 
 
-   console.log('PCM container: ' + scope.myPCMContainer);
-   console.log('PCM: ' + scope.myPCMContainer.pcm);
-   console.log('PCM: ' + scope.myPCMContainer.pcm.name);
 
-  }));
+ }));*/
+
 
 });
